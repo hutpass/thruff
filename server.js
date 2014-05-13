7ac49b25593fb5f64387bb88ac4bda07f5bcf363
Stuart P. Bentley <stuart@testtrack4.com>
2014-05-13 05:01:34 -0400

    Add redirects

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ function getTarget(domain) {
   return record && record.target;
 }
 
+function getRedirect(domain) {
+  var record = getDomain(domain);
+  return record && record.redirect;
+}
+
 function updateDomains(message) {
   // there should really only be one domain per message but
   var msgDomains = Object.keys(message);
@@ -46,8 +51,15 @@ function updateDomains(message) {
       // Create any new domain
       if (!domains[domain]) domains[domain] = {};
       // Update any target
-      if (message[domain].target)
+      if (message[domain].target) {
         domains[domain].target = message[domain].target;
+        delete domains[domain].redirect;
+      }
+      // If not target but redirect, set redirect instead
+      else if (message[domain].redirect) {
+        domains[domain].redirect = message[domain].redirect;
+        delete domains[domain].target;
+      }
       // Create credentials for any key + cert pairs
       if (message[domain].key && message[domain].cert)
         domains[domain].credentials = crypto.createCredentials({
@@ -79,33 +91,39 @@ var proxy = require('http-proxy').createProxyServer({
   xfwd: true, secure: false
 });
 
-function respondError(err, req, res){
+function respondError(err, req, res) {
   res.statusCode = 500;
   // TODO: handle error object
   res.end();
 }
 
-function respondNotFound(req, res){
+function respondNotFound(req, res) {
   res.statusCode = 404;
   res.end();
 }
 
+function respondRedirect(req, res, url) {
+  res.statusCode = 301;
+  res.setHeader('Location', url);
+  res.end();
+}
+
 function forwardRequest(req, res) {
-  var target = getTarget(req.headers.host);
-  if (target) return proxy.web(req, res, {target:target});
+  var record = req.headers.host && getDomain(req.headers.host);
+  if (record && record.target)
+    return proxy.web(req, res, {target:record.target});
+  else if (record && record.redirect)
+    return respondRedirect(req, res, record.redirect);
   else return respondNotFound(req, res);
 }
 
 function redirectToHttps(req, res) {
   var host = req.headers.host;
   // If they're asking for a name we're proxying
-  if (host && getTarget(host)) {
-    res.statusCode = 301;
-    res.setHeader('Location', 'https://' + host + req.url);
-    res.end();
+  if (host && getDomain(host)) {
+    return respondRedirect('https://' + host + req.url);
   } else {
-    // They're not following the HTTP/1.1 spec - should this just be a 400?
-    respondNotFound(req, res);
+    return respondNotFound(req, res);
   }
 }
 
